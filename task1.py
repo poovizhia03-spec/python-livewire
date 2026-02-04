d3a972871fe369934100f345162682299f149b8a
poovizhia03-spec <poovizhia03@gmail.com>
2026-02-04 11:34:49 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,77 @@
+#task1
+'''print("Hello world")'''
+
+#task2
+'''a=20
+b=30
+print("add:",a+b)
+print("sub:",a-b)
+print("mult:",a*b)
+print("div:",a/b)
+print("mod:",a%b)'''
+
+#task3
+'''num=int(input("enter the number:"))
+num=num**0.5
+print(num)'''
+
+#task4
+'''base=5
+height=7
+a=0.5*base*height
+print(a)'''
+
+#task5
+'''a=10
+b=20
+f=a**2 + b**2 +2*a*b
+print("a+b**2:",f)
+d=a**2 +b**2 -2*a*b
+print("a-b**2:",d)
+s=(a+b)*(a-b)
+print(s)'''
+
+#task6
+
+a=20
+b=30
+print("swap variable:",b)
+print("swap variable:",a)
+c=a
+a=b
+b=c
+
+#task 7
+'''kilometer =70
+a=kilometer*0.63
+print(a)
+
+'''
+
+#task 8
+'''cels=67
+f=(cels*1.8)+32
+print("celeius convert to fahrenheit",f)'''
+
+
+#task 9
+'''a=4567
+print(a%10)'''
+
+#task 10
+'''b=3427
+print(b%100)'''
+
+#task 11
+a=int(input("enter the number:"))
+a//=100
+c=a%10
+print("square of 6:",c**2)
+
+
+
+
+
+
+
+
